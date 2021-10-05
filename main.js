2e1cf87c24a9aa595fbf499dfece29b3984aacc9
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-10-06 00:34:40 +0900

    iris

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let HID_NODE;
 let OUT_NODE = 1;
 
 const ETA = 0.5;
-const THRESHOLD = 20000;
+const THRESHOLD = 100000;
 
 const sigmoid = x => 1 / (1 + Math.exp(-x));
 const dsigmoid = x => x * (1 - x);
@@ -18,6 +18,7 @@ const dfmax = x => (0 < x) ? 1 : 0;
 
 let epoch; //学習回数
 let DATA_LEN; //学習データ数
+let TEST_LEN;
 let fError;
 
 let hid = [];
@@ -29,6 +30,8 @@ let x; //学習データ+バイアス
 let t; //教師信号
 let v = []; //v[HID_NODE][IN_NODE]
 let w = []; //w[OUT_NODE][HID_NODE]
+let x2; //テストデータ+バイアス
+let t2; //テスト教師信号
 
 //乱数生成
 //const frandWeight = () => math.random(0.5, 1.0); // 0.5 <= x < 1.0
@@ -75,19 +78,22 @@ const outputNode = (arrInput) => {
  */
 const printResult = () => {
     console.log();
-    for (let i = 0; i < DATA_LEN; i++) {
+    for (let i = 0; i < TEST_LEN; i++) {
         //calculateNode(i);
-        const ret = outputNode(x[i]);
+        const ret = outputNode(x2[i]);
         const fout = ret[0].toFixed(2);
-        console.log(`${t[i][0]}: ${fout}`);
+        console.log(`${t2[i][0]}: ${fout}`);
     }
 }
 /**
  * Main
  */
 {
-    const strJson = fs.readFileSync('./json/cell30.json', 'utf8'); //xor | cell30
+    const strJson = fs.readFileSync('./json/iris-train.json', 'utf8'); //xor | cell30
     const arrHsh = JSON.parse(strJson);
+
+    const strJson2 = fs.readFileSync('./json/iris-test.json', 'utf8');
+    const arrHsh2 = JSON.parse(strJson2);
 
     x = _.map(arrHsh, hsh => {
         let arrBuf = hsh.input;
@@ -96,9 +102,17 @@ const printResult = () => {
     });
     t = _.map(arrHsh, hsh => hsh.output);
 
+    x2 = _.map(arrHsh2, hsh => {
+        let arrBuf = hsh.input;
+        arrBuf.push(frandBias()); //add input bias
+        return arrBuf;
+    });
+    t2 = _.map(arrHsh2, hsh => hsh.output);
+
     IN_NODE = arrHsh[0].input.length; //入力ノード数決定（バイアス含む）
     HID_NODE = IN_NODE + 1; //隠れノード数決定
     DATA_LEN = x.length;
+    TEST_LEN = x2.length;
 
     for (let i = 0; i < HID_NODE; i++) {
         v.push([]);
@@ -157,7 +171,7 @@ const printResult = () => {
             epoch = epoch + 0; //debug
         }
 
-        if (epoch % 1000 === 0) {
+        if (epoch % 10000 === 0) {
             const s = epoch + '';
             console.log(`${s.padStart(5)}: ${fError.toFixed(6)}`);
         }
